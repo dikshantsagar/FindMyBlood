a402e3feb57b069883fe36e5b31b7f66825decf9
Dikshant Sagar <sagar.dikshant@gmail.com>
2022-10-08 15:05:22 -0700

    Update settings.py

--- blood/settings.py
+++ blood/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, "static"),
