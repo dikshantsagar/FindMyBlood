970fd2f23e932a2872e05268be1c58bbcbc13bfc
Dikshant Sagar <sagar.dikshant@gmail.com>
2022-10-25 19:15:05 -0700

    Revert "Integrated Push Notifications and dynamic changes in project"
    This reverts commit cd524963be3c1bd299ccf2be0eb79fa7a170bb07.

--- blood/views.py
+++ blood/views.py
@@ -10,7 +10,6 @@ import numpy as np
 import pandas as pd
 import requests
 import json
-import os
 
 
 from .utils import distance
@@ -22,13 +21,9 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-firebase_creds = os.environ['firebase_creds']
-fcm_creds = os.environ['fcm_creds']
-fcm_vapidKey = os.environ['fcm_vapidKey']
-fcm_token = os.environ['fcm_token']
-
-cred = credentials.Certificate(json.loads(firebase_creds))
-pushCred = credentials.Certificate(json.loads(fcm_creds))
+cred = credentials.Certificate("../config.json")
+with open('../push-config.json', 'r') as f:
+    pushCred = json.load(f)  
 
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://blood-ed205-default-rtdb.firebaseio.com',
@@ -48,7 +43,8 @@ def index(request):
     # #print(type(ref.get()))
     # # ref.update({'user2':{'bloodgroup': 'A+', 'location': '(230,430)', 'name': 'kumar', 'password': 'pass123'}})
     # ref.update({'user1':{'bloodgroup': 'A+', 'location': '(230,430)', 'email': 'rahul@gmail.com', 'password': 'pass123'}})
-    return render(request,'index.html',pushCred)
+    
+    return render(request,'index.html')
 
 
 @csrf_exempt
@@ -82,9 +78,8 @@ def signupworker(request):
         gender = request.POST.get('gender')
         age = request.POST.get('age')
         utype = request.POST.get('type')
-        pushToken  = request.POST.get('pushToken')
 
-        dic = {'bloodgroup' : group, 'location':'('+lat+','+longi+')', 'name':name, 'email':email, 'password':password, 'phone':phone, 'gender':gender,'age':age,'type':utype,'push-token':pushToken}
+        dic = {'bloodgroup' : group, 'location':'('+lat+','+longi+')', 'name':name, 'email':email, 'password':password, 'phone':phone, 'gender':gender,'age':age,'type':utype}
 
         ref = db.reference('/users/')
         userind = int(list(sorted(list(ref.get()))[-1])[-1]) + 1
@@ -124,7 +119,6 @@ def signin(request):
     if request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
-        pushToken = request.POST.get('pushToken')
 
         ref = db.reference('/users/')
         users = ref.get()
@@ -133,7 +127,6 @@ def signin(request):
 
         for i in users:
             if(users[i]['email'] == email and users[i]['password'] == password):
-                ref.child(i).update({'push-token':pushToken})
                 return home(request,users[i])
                 #return redirect('home',users[i])
         
@@ -187,14 +180,14 @@ def showFirebaseJS(request):
     data='importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");' \
          'importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js"); ' \
          'var firebaseConfig = {' \
-         '        apiKey: "{pushCred[`apiKey`]}",' \
-         '        authDomain: "{pushCred[`authDomain`]}",' \
-         '        databaseURL: "{pushCred[`databaseURL`]}",' \
-         '        projectId: "{pushCred[`projectId`]}",' \
-         '        storageBucket: "{pushCred[`storageBucket`]}",' \
-         '        messagingSenderId: "{pushCred[`messagingSenderId`]}",' \
-         '        appId: "{pushCred[`appId`]}",' \
-         '        measurementId: "{pushCred[`measurementId`]}"' \
+         '        apiKey: "{pushCred["apiKey"]}",' \
+         '        authDomain: "{pushCred["authDomain"]}",' \
+         '        databaseURL: "{pushCred["databaseURL"]}",' \
+         '        projectId: "{pushCred["projectId"]}",' \
+         '        storageBucket: "{pushCred["storageBucket"]}",' \
+         '        messagingSenderId: "{pushCred["messagingSenderId"]}",' \
+         '        appId: "{pushCred["appId"]}",' \
+         '        measurementId: "{pushCred["measurementId"]}"' \
          ' };' \
          'firebase.initializeApp(firebaseConfig);' \
          'const messaging=firebase.messaging();' \
@@ -212,7 +205,7 @@ def showFirebaseJS(request):
 
 def sendPushNotification(registration_ids , message_title , message_desc):
     # fcm_api : Can be found in firebase console > cloud messaging > Server key
-    fcm_api = fcm_token
+    fcm_api = "AAAAxy_qm84:APA91bGFhA8Vs2oZKgY8IWazGaf-T5AgtCG32o4miKPWPrcAh3bbIIKtPPvrM4RC9k0D6vrSRnGwAcwyvI7blwSgDX0ZMElGf4Y0ha3ulsDCPsg9-GAACAa6E1WC9DiEnIhh5USrOdFs"
     url = "https://fcm.googleapis.com/fcm/send"
     
     headers = {
@@ -233,15 +226,10 @@ def sendPushNotification(registration_ids , message_title , message_desc):
     result = requests.post(url,  data=json.dumps(payload), headers=headers )
     print(result.json())
 
-def sendPush(receiver,donor):
-    dref = db.reference('/users/user'+str(donor))
-    rref = db.reference('/users/user'+str(receiver))
-    donorToken = dref.child('push-token').get()
-    donorName = dref.child('name').get()
-    receiverName = rref.child('name').get()
+def sendPush(request):
     # registration : Fcm token of user whom you need to send the notificaiton 
-    resgistration  = [donorToken]
-    sendPushNotification(resgistration , 'New Blood Request from '+receiverName,'Hello '+donorName+', You have received new blood request from '+receiverName)
+    resgistration  = ["cg2lA75aH1Okm7g-RaqtAl:APA91bGabNc3WemuriyzETm9f7Sy3A6GcW6GbzPlTfWHYTy4y1_bWMTnLQkZNFalL__3RN_gZdYoIZW3KjE2t-ZsC9mu82wYoXH8Eu33kichmOLbpHiOAP2dpTLc-totIolH3jjeQp2f"]
+    sendPushNotification(resgistration , 'You have received new blood request' , 'New Blood Request')
     return HttpResponse("sent")
 
 @csrf_exempt
@@ -258,7 +246,6 @@ def sendrequest(request):
         ref = db.reference('/users/user'+str(donid)+"/requests/")
         dbref = ref.get()
         exists = 0
-        sendPush(recid,donid)
         for i in dbref:
             print(dbref[i])
             if dbref[i]['from'] == recname:
